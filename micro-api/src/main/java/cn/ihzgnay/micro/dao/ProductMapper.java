f9216b29fd7102dfbb9d6572292df02424938033
Yangzzzzz <1143802555@qq.com>
2020-03-21 18:10:41 +0800

    init

---
+++ micro-api/src/main/java/cn/ihzgnay/micro/dao/ProductMapper.java
@@ -0,0 +1,7 @@
+package cn.ihzgnay.micro.dao;
+
+import cn.ihzgnay.micro.pojo.Product;
+import cn.ihzgnay.micro.tool.tk.TkMapper;
+
+public interface ProductMapper extends TkMapper<Product> {
+}
---
+++ micro-api/src/main/java/cn/ihzgnay/micro/pojo/Product.java
@@ -0,0 +1,57 @@
+package cn.ihzgnay.micro.pojo;
+
+import javax.persistence.*;
+
+public class Product {
+    @Id
+    @Column(name = "productId")
+    private Long productid;
+
+    @Column(name = "productName")
+    private String productname;
+
+    @Column(name = "productDesc")
+    private String productdesc;
+
+    /**
+     * @return productId
+     */
+    public Long getProductid() {
+        return productid;
+    }
+
+    /**
+     * @param productid
+     */
+    public void setProductid(Long productid) {
+        this.productid = productid;
+    }
+
+    /**
+     * @return productName
+     */
+    public String getProductname() {
+        return productname;
+    }
+
+    /**
+     * @param productname
+     */
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    /**
+     * @return productDesc
+     */
+    public String getProductdesc() {
+        return productdesc;
+    }
+
+    /**
+     * @param productdesc
+     */
+    public void setProductdesc(String productdesc) {
+        this.productdesc = productdesc;
+    }
+}
